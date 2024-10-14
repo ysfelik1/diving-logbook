95ee1bc0e08fe69158961c8d3e3a5113c12f00c1
Yusuf Elik <ysfelik1@gmail.com>
2024-10-14 11:28:25 +0200

    Set Express server

---
+++ server.js
@@ -0,0 +1,11 @@
+import { createServer } from 'http';
+import app from './app.js'; // Make sure app.js exists and is properly exported
+
+const port = process.env.PORT || 3000;
+
+// Pass the Express app as the request handler to createServer
+const server = createServer(app);
+
+server.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
