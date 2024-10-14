31f869c6c2aa4e006286d7698c0b01aad6e02966
Yusuf Elik <ysfelik1@gmail.com>
2024-10-14 16:31:34 +0200

    Add fields in POST methods

--- api/routes/users.js
+++ api/routes/users.js
@@ -1,38 +1,46 @@
-import express from 'express';
-const router = express.Router()
+import express from "express";
+const router = express.Router();
 
-router.get('/',(req,res,next)=>{
+router.get("/", (req, res, next) => {
+  res.status(200).json({
+    message: "GET /users",
+  });
+});
+
+router.get("/:id", (req, res, next) => {
+  const id = req.params.id;
+  if (id) {
     res.status(200).json({
-        message:'GET /users'
-    })
-})
+      message: "Parameter is " + id,
+    });
+  }
+});
 
-router.get('/:id',(req,res,next)=>{
-    const id=req.params.id;
-    if(id){
-        res.status(200).json({
-            message:'Parameter is '+id
-        })
-    }
-})
+router.post("/", (req, res, next) => {
+  const user = {
+    id: req.body.id,
+    name: req.body.name,
+    lastnName: req.body.lastnName,
+    currentLevel: req.body.currentLevel,
+    country: req.body.country,
+    birthOfday: req.body.birthOfday,
+  };
 
-router.post('/',(req,res,next)=>{
-    res.status(201).json({
-        message:'POST user'
-    })
-})
+  res.status(201).json({
+    message: "New created user:" + " " + user.id,
+  });
+});
 
-router.patch('/:id',(req,res,next)=>{
-    res.status(200).json({
-        message:'Update user'
-    })
-})
+router.patch("/:id", (req, res, next) => {
+  res.status(200).json({
+    message: "Update user",
+  });
+});
 
-router.delete('/:id',(req,res,next)=>{
-    res.status(200).json({
-        message:'Delete user'
-    })
-})
-
+router.delete("/:id", (req, res, next) => {
+  res.status(200).json({
+    message: "Delete user",
+  });
+});
 
 export default router;
