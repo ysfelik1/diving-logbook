6ec92bb2a47621cc5b6fa1f1e5b6a5a5a5aef33f
Yusuf Elik <ysfelik1@gmail.com>
2024-10-14 15:47:47 +0200

    add error messages

--- app.js
+++ app.js
@@ -1,13 +1,27 @@
-import express from 'express';
+import express from "express";
 
 const app = express();
 
-import usersRoutes from './api/routes/users.js'; 
-import divesRoutes from './api/routes/dives.js'; 
-
+import usersRoutes from "./api/routes/users.js";
+import divesRoutes from "./api/routes/dives.js";
 
 // Your middleware, routes, etc.
-app.use('/users',usersRoutes);
-app.use('/dives',divesRoutes);
+app.use("/users", usersRoutes);
+app.use("/dives", divesRoutes);
+
+app.use((req, res, next) => {
+  const error = new Error("Not Found");
+  error.status = 404;
+  next(error);
+});
+
+app.use((error, req, res, next) => {
+  res.status(error.status || 500);
+  res.json({
+    error: {
+      message: error.message,
+    },
+  });
+});
 
 export default app;
