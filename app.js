0bddd668d3aee437ef9742b0b52482ceb11c5165
Yusuf Elik <ysfelik1@gmail.com>
2024-10-14 17:11:30 +0200

    Add basic CORS handling

--- app.js
+++ app.js
@@ -15,6 +15,19 @@ app.use("/users", usersRoutes);
 app.use("/dives", divesRoutes);
 
 app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  if (req.method === "OPTIONS") {
+    res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET");
+    return res.status(200).json({});
+  }
+  next();
+});
+
+app.use((req, res, next) => {
   const error = new Error("Not Found");
   error.status = 404;
   next(error);
